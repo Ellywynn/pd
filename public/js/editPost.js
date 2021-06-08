598961a77b73b72652810c224d36255d0cd1e0b2
Ellywynn <whatisyouandme@mail.ru>
2021-06-08 15:44:51 +0300

    post content validation

--- public/js/editPost.js
+++ public/js/editPost.js
@@ -1,10 +1,20 @@
 $(document).ready(function() {
     $('#summernote').summernote({
-        lang:'ru-RU',
-        minHeight: 500,
-        maxHeight: null,
-        focus: true,
-        placeholder: 'Расскажите что-нибудь интересное!',
-        fontNames: ['Arial', 'Times New Roman', 'Helvetica']
+        minHeight: 300,
+        maxHeight: 600,
+        placeholder: 'Расскажите что-нибудь интересное!'
+    });
+
+    // валидация на пустое поле summernote
+    $('.summernote-form').submit(function(event) {
+        
+        if (!$('#summernote').summernote('isEmpty')) {
+            alert('valid');
+            return;
+        }
+
+        alert('Заполните поле контента!');
+
+        event.preventDefault();
     });
 });
